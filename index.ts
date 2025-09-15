47260ad06a246464f321e78c29ea66eeb2e2ff91
umarnjagitech <omarnjagi@gmail.com>
2025-09-15 17:58:10 +0300

    Update .gitignore and project files

--- index.ts
+++ index.ts
@@ -1,11 +1,25 @@
-import { generateText } from "ai";
-// import { generateText } from "@ai-sdk/google";
+import { stepCountIs, streamText } from "ai";
 import { google } from "@ai-sdk/google";
+import { SYSTEM_PROMPT } from "./prompts";
+import { getFileChangesInDirectoryTool } from "./tools";
 
-// Specify the model to use for generating text and a prompt
-const { text } = await generateText({
-    model: google("gemini-2.5-flash"),
-    prompt: "What is an AI agent?",
-});
+const codeReviewAgent = async (prompt: string) => {
+  const result = streamText({
+    model: google("models/gemini-2.5-flash"),
+    prompt,
+    system: SYSTEM_PROMPT,
+    tools: {
+      getFileChangesInDirectoryTool: getFileChangesInDirectoryTool,
+    },
+    stopWhen: stepCountIs(10),
+  });
 
-console.log(text);
+  for await (const chunk of result.textStream) {
+    process.stdout.write(chunk);
+  }
+};
+
+// Specify which directory the code review agent should review changes in your prompt
+await codeReviewAgent(
+  "Review the code changes in '../th3ch1ef4g3nt' directory, make your reviews and suggestions file by file",
+);
---
+++ tools.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+import { tool } from "ai";
+import { simpleGit } from "simple-git";
+
+const fileChange = z.object({
+    rootDir: z.string().min(1).describe("The root directory"),
+});
+
+type FileChange = z.infer<typeof fileChange>;
+
+const excludeFiles = ["dist", "bun.lock"];
+
+async function getFileChangesInDirectory({ rootDir }: FileChange) {
+  const git = simpleGit(rootDir);
+  const summary = await git.diffSummary();
+  const diffs: { file: string; diff: string }[] = [];
+
+  for (const file of summary.files) {
+    if (excludeFiles.includes(file.file)) continue;
+    const diff = await git.diff(["--", file.file]);
+    diffs.push({ file: file.file, diff });
+  }
+
+  return diffs;
+}    
+
+export const getFileChangesInDirectoryTool = tool({
+    description: "Gets the code changes made in given directory",
+    inputSchema: fileChange,
+    execute: getFileChangesInDirectory,
+  });
